9a608bd05394a75c2e13faae9ac42213582d6dbf
WilliamMoller <william.moller.te16d@gmail.com>
2017-12-01 10:21:47 +0100

    login
    fixat namn och lösenord för login-formuläret

--- Uppgifter/login/login.php
+++ Uppgifter/login/login.php
@@ -13,11 +13,12 @@
 	
 	</head>
 	<body>
-	
-	
-	
-	
-	
-	
+		<label><b>First_name </b></label>
+		<input type="text" placeholder="Enter First_name" name="Fname" required>
+		<br>		
+		<label><b>Password</b></label>
+		<input type="password" placeholder="Enter Password" name="psw" required>
+		<br>
+		<button type="submit" class="signupbtn">Submit</button>
 	</body>
 </html>
