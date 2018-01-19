1da4d72689ceda0d3ad015c651227960a347d40e
WilliamMoller <william.moller.te16d@gmail.com>
2018-01-19 11:07:45 +0100

    uppgift 2
    hållt på för att fösöka få allt att fungera och nu gör det det. om man håller på bilden nu så blider större och när man drar bort så går den tillbaks till orginal

--- Javascript/index.php
+++ Javascript/index.php
@@ -9,8 +9,24 @@
 			alert("HALLOJ");
 		}
 	
+		function bigImg(x) {
+			x.style.height = "150px";
+			x.style.width = "250px";
+		}
+
+		function normalImg(x) {
+			x.style.height = "50px";
+			x.style.width = "70px";
+		}
+		
+		
 	</script>
 	<body>
 		<button onClick="meddelande()"> uppgift 1 </button>
+		<br>
+		<br>
+		<img src="ontopoftheworld.jpg" alt="Uppgift 2" onmouseover="bigImg(this)" onmouseleave="normalImg(this)" />
+		<br>
+		
 	</body>
 </html>
